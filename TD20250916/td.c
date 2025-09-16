ebcf24822c191f3a3efb1f2de8b603c592cd6aed
BuffinLouis <louis.buffin@heig-vd.ch>
2025-09-16 12:35:02 +0000

    Initialisation differentes variables

---
+++ TD20250916/td.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	__uint16_t num_students = 23;
+	int Mean_account_value = -42;
+	float temperature = 19.9;		//32 bits
+	double pi = 3.14159265359;
+	char poid_de_tadaronne[] = "oublie frere";
+}
