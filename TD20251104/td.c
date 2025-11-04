b1f259190474e2b15835a8decb3e8ee94733347b
BuffinLouis <louis.buffin@heig-vd.ch>
2025-11-04 13:19:52 +0000

    test front

--- TD20251104/td.c
+++ TD20251104/td.c
@@ -14,8 +14,24 @@ int main(int argc, const char *argv[])
 
 	do
 	{
-		printf("Enter a number between [10..20]: ");
-		scanf("%d", &i);
+		int ret = 0;
+
+		printf("Enter a number between [%d..%d]: ", V_MIN, V_MAX);
+		ret = scanf("%d", &i);
+		// si le scanf ne peut pas mettre a jour variable.
+		if (ret != 1)
+		{
+			// Vider le buffer clavier
+			// Méthode 1
+			while (getchar() != '\n') {}
+			// Méthode 1
+			// char c = 0;
+			// do
+			// {
+			// 	c = getchar();
+			// } while (c != '\n');
+		}
+
 	} while ((i < V_MIN) || (i > V_MAX));
 
 	printf("i=%d\n", i);
